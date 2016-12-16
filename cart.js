5962407534f87188454aed61a4ec2bbd7afca189
MikeVayda <9mvayda@stpatrick.org>
2016-12-16 10:51:07 -0600

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,32 +1,30 @@
 class Cart{
   //What is the first part of every class? Type it below.
-  constructor(l,q){
-    this.itemList = l;
-    this.itemQuantity = q;
-  }
+constructor(i,q){
+  this.itemList = i;
+  this.itemquality = q;
+}
+
 
   //Type the instance functions below this comment.
-
-  addItem(i,q){
-    this.itemList.push(i);
-    this.itemQuantity.push(q);
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemquality.push(q);
+}
+totalCart(){
+  let total = 0;
+  for(a=0;a<this.itemlist.length;a++){
+    total += this.itemList.[a].price*this.itemquality[a];
   }
-
-  totalCart(){
-    let total = 0;
-    for(a=0;a<this.itemList.length;a++){
-      total += this.itemList[a].price*this.itemQuantity[a];
+  return total;
+}
+subCart(days){
+  let subCart = new Cart([],[]);
+  for(b=0;b<this.itemList.length;a++){
+    if(this.itemList[b].shipping == days){
+      subCart.push([b]);
     }
-    return total;
   }
-
-  subCart(days){
-    let subCart = new Cart([],[]);
-    for(b=0;b>this.itemList.length;a++){
-      if(this.itemList[b].shipping == days){
-        subCart.push([b]);
-      }
-    }
-    return subCart;
-  }
+  return subCart
+}
 }
